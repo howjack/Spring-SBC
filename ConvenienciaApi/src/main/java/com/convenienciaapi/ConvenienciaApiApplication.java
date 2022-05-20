48122d684950a7bb79227bb4801fa546bad5495e
Phelipe Alves <phelipe44@gmail.com>
2022-05-19 21:14:59 -0300

    Feat/ Build spring project

---
+++ ConvenienciaApi/src/main/java/com/convenienciaapi/ConvenienciaApiApplication.java
@@ -0,0 +1,13 @@
+package com.convenienciaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvenienciaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvenienciaApiApplication.class, args);
+	}
+
+}
---
+++ ConvenienciaApi/src/test/java/com/convenienciaapi/ConvenienciaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.convenienciaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvenienciaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
